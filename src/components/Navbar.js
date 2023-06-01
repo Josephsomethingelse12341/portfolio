28db46ecccb2bf0041b8f47bd36b84f4cb8c448e
Josephsomethingelse12341 <joseph.degois17@gmail.com>
2023-06-01 18:43:38 -0400

    messing around with color palettes and grids in project pg

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -36,13 +36,13 @@ export const NavBar = () => {
   return (
     <Router>
       <Navbar expand="md" className={scrolled ? "scrolled" : ""} > 
-        <Container>
+        <Container class="">
           <Navbar.Collapse id="basic-navbar-nav">
-            <nav class="bg-teal-500 w-full">
+            <nav class="bg-gradient-to-b from-teal-500 from-90% to-rose-500  w-full">
               <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
                 <div class="hidden w-full md:block md:w-auto" id="navbar-default">
                   <Nav class="font-medium text-lg flex flex-col p-6 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white dark:bg-teal-400 md:dark:bg-teal-500 dark:border-teal-300">
-                      <a href="/" className={activeLink === 'home' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('home')} class="block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white md:dark:text-blue-500" aria-current="page">Home</a>
+                      <a href="/" className={activeLink === 'home' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('home')} class="block py-2 pl-3 pr-4 text-white  rounded md:bg-transparent  md:p-0 dark:text-white ">Home</a>
                       <a href="/about" className={activeLink === 'about' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('about')} class="hover:transition-all block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">About</a>
                       <a href="/projects" className={activeLink === 'projects' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('contact')} class="hover:transition-all block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Projects</a>
                       <a href="/contact" className={activeLink === 'contact' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('contact')} class="hover:transition-all block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Contact</a>
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,19 +1,40 @@
+
+
 export const Projects = () => {
     return (
-        <div class="mt-8">
+        <div class="bg-rose-500 min-h-screen py-5">
+        <div class=" mt-8 my-2 grid grid-cols-2 mx-40 shadow-lg">
             <div class="bg-white rounded overflow-hidden shadow-lg">
-                <img src="https://picsum.photos/200/300" alt="Project 1"/>
+                <img src="https://wiki.teamfortress.com/w/images/thumb/a/af/Demoman_taunt_laugh.png/350px-Demoman_taunt_laugh.png" alt="Project 1" class="w-full h-32 sm:h-48"/>
                 <div>
-                    <span class="font-bold">Project 1</span>
-                    <span class="block">stuff, example text</span>
+                    <span class="font-bold">The Warrior</span>
+                    <span class="block">example text</span>
+                </div>
+            </div>
+            <div class="bg-white rounded overflow-hidden shadow-lg">
+                <img src="https://wiki.teamfortress.com/w/images/thumb/a/af/Demoman_taunt_laugh.png/350px-Demoman_taunt_laugh.png" alt="Project 1" class="w-full h-32 sm:h-48"/>
+                <div>
+                    <span class="font-bold">The Warrior</span>
+                    <span class="block">example text</span>
+                </div>
+            </div>
+            <div class="bg-white rounded overflow-hidden shadow-lg">
+                <img src="https://wiki.teamfortress.com/w/images/thumb/a/af/Demoman_taunt_laugh.png/350px-Demoman_taunt_laugh.png" alt="Project 1" class="w-full h-32 sm:h-48"/>
+                <div>
+                    <span class="font-bold">The Warrior</span>
+                    <span class="block">example text</span>
+                </div>
+            </div>
+            <div class="bg-white rounded overflow-hidden shadow-lg">
+                <img src="https://wiki.teamfortress.com/w/images/thumb/a/af/Demoman_taunt_laugh.png/350px-Demoman_taunt_laugh.png" alt="Project 1" class="w-full h-32 sm:h-48"/>
+                <div>
+                    <span class="font-bold">The Warrior</span>
+                    <span class="block">example text</span>
                 </div>
             </div>
         </div>
-        <div class="space-y-4 flex flex-col ...">
-            <div class="container block">Project 1</div>
-            <span class="block">Project 2</span>
-            <span class="block">Project 3</span>
         </div>
+        
     )
 }
 
