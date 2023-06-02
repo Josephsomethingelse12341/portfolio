cf4644e1c5a73cf719a4b7a843dd17ecda1fb500
Josephsomethingelse12341 <joseph.degois17@gmail.com>
2023-06-02 17:01:42 -0400

    fixed gaps and adjusted background

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -17,25 +17,25 @@ export const Contact = () => {
     };
 
     return (
-        <div>
+        <div class="bg-blue-200 min-h-screen">
             <div className="container mx-auto px-6 py-8">
                 <h3 className="text-gray-700 text-3xl font-medium">Contact Me</h3>
                 <form ref={form} onSubmit={sendEmail}>
                     <div className="grid grid-cols-1 sm:grid-cols-2 gap-6 mt-4">
                         <div>
-                            <input type="text" name="name" id="name" placeholder="Your Name" className="border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 rounded-md shadow-sm mt-1 block w-full"/>
+                            <input type="text" name="name" id="name" placeholder=" Your Name" className="border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 rounded-md shadow-lg mt-1 block w-full"/>
                         </div>
                         <div>
-                            <input type="email" name="email" id="email" placeholder="Your Email Address" className="border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 rounded-md shadow-sm mt-1 block w-full"/>
+                            <input type="email" name="email" id="email" placeholder=" Your Email Address" className="border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 rounded-md shadow-lg mt-1 block w-full"/>
                         </div>
                         <div>
-                            <input type="text" name="subject" id="subject" placeholder="Subject" className="border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 rounded-md shadow-sm mt-1 block w-full"/>
+                            <input type="text" name="subject" id="subject" placeholder=" Subject" className="border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 rounded-md shadow-lg mt-1 block w-full"/>
                         </div>
                         <div>
-                            <textarea name="message" id="message" rows="4" placeholder="Your Message" className="border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 rounded-md shadow-sm mt-1 block w-full"></textarea>
+                            <textarea name="message" id="message" rows="4" placeholder=" Your Message" className="border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 rounded-md shadow-lg mt-1 block w-full"></textarea>
                         </div>
                         <div className="text-right col-span-2">
-                            <button type="submit" value="Send Message" className="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-500 focus:outline-none" >Send Message</button>
+                            <button type="submit" value="Send Message" className="inline-flex justify-center py-2 px-4 border border-transparent shadow-lg text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-500 focus:outline-none" > Send Message</button>
                         </div>
                     </div>
                 </form>
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -2,30 +2,30 @@
 
 export const Projects = () => {
     return (
-        <div class="bg-rose-500 min-h-screen py-5">
-        <div class=" mt-8 my-2 grid grid-cols-2 mx-40 shadow-lg">
-            <div class="bg-white rounded overflow-hidden shadow-lg">
+        <div class="bg-blue-200 min-h-screen py-5"> 
+        <div class=" mt-8 my-2 grid grid-cols-2 mx-40 ">
+            <div class="bg-white rounded overflow-hidden shadow-lg m-4">
                 <img src="https://wiki.teamfortress.com/w/images/thumb/a/af/Demoman_taunt_laugh.png/350px-Demoman_taunt_laugh.png" alt="Project 1" class="w-full h-32 sm:h-48"/>
                 <div>
                     <span class="font-bold">The Warrior</span>
                     <span class="block">example text</span>
                 </div>
             </div>
-            <div class="bg-white rounded overflow-hidden shadow-lg">
+            <div class="bg-white rounded overflow-hidden shadow-lg m-4">
                 <img src="https://wiki.teamfortress.com/w/images/thumb/a/af/Demoman_taunt_laugh.png/350px-Demoman_taunt_laugh.png" alt="Project 1" class="w-full h-32 sm:h-48"/>
                 <div>
                     <span class="font-bold">The Warrior</span>
                     <span class="block">example text</span>
                 </div>
             </div>
-            <div class="bg-white rounded overflow-hidden shadow-lg">
+            <div class="bg-white rounded overflow-hidden shadow-lg m-4">
                 <img src="https://wiki.teamfortress.com/w/images/thumb/a/af/Demoman_taunt_laugh.png/350px-Demoman_taunt_laugh.png" alt="Project 1" class="w-full h-32 sm:h-48"/>
                 <div>
                     <span class="font-bold">The Warrior</span>
                     <span class="block">example text</span>
                 </div>
             </div>
-            <div class="bg-white rounded overflow-hidden shadow-lg">
+            <div class="bg-white rounded overflow-hidden shadow-lg m-4">
                 <img src="https://wiki.teamfortress.com/w/images/thumb/a/af/Demoman_taunt_laugh.png/350px-Demoman_taunt_laugh.png" alt="Project 1" class="w-full h-32 sm:h-48"/>
                 <div>
                     <span class="font-bold">The Warrior</span>
