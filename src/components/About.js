1f5d7b7b0a6161d9bef35a5ac9f75a093159649e
Josephsomethingelse12341 <joseph.degois17@gmail.com>
2023-06-05 05:24:50 -0400

    updated about page, added social links.

--- src/components/About.js
+++ src/components/About.js
@@ -1,4 +1,6 @@
 import profPic from '../assets/img/header-img.png'
+import navIcon1 from '../assets/img/nav-icon1.svg'
+import navIcon2 from '../assets/img/nav-icon2.svg'
 
 export const About = () => {
     return (
@@ -10,7 +12,17 @@ export const About = () => {
                             <img src={profPic} alt="Profile pic" class="rounded-full w-32 h-32 "/>
                         </div>
                         <h1 class="text-lg text-bold font-[Bahnschrift]">Joseph De Gois</h1>
-                        <h2 class="text-md text-gray-700">Toronto, Ontario, Canada</h2>
+                        <h2 class="text-md text-gray-700 text-bold font-[Bahnschrift]">Toronto, Ontario, Canada</h2>
+                        <p class="font-[Bahnschrift] text-sm p-4">I am a Front end Developer. I specialize in designing and building the user interface of both websites and applications.</p>
+                        <div className="flex items-center justify-center pt-10 flex-cols-2  my-2">
+                            <a href="https://www.linkedin.com/in/joseph-de-gois-020651220/" target="_blank" rel="noreferrer"><img src={navIcon1} class="w-10 h-10 p-1 mr-3 border-2 "alt="" /></a>
+                            <a href="https://github.com/Josephsomethingelse12341" target="_blank" rel="noreferrer"><img src={navIcon2} class="w-10 h-10 p-1 mr-3 border-2"alt="" /></a>
+                        </div>
+                    </div>
+                </div>
+                <div class="flex items-center justify-center ">
+                    <div class="bg-blue-300 w-1/3 mt-10 rounded-lg shadow-2xl">
+                        <h1 class="pt-5 text-lg text-bold font-[Bahnschrift]">Skills</h1>
                     </div>
                 </div>
             </div>
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -11,7 +11,7 @@ export const Projects = () => {
                     <img src={projImg1} alt="Project 1" class="w-full h-32 sm:h-48"/>
                     <div>
                         <span class="font-bold">Quizhoot</span>
-                        <span class="block text-sm text-gray-500">This project was my final capstone project in college. It's an app that contains quizzes and forums which provides a community based learning environment. My contribution was mostly on the forum aspect in terms of front end and backend, also helped out with the general frontend.</span>
+                        <span class="block text-sm text-gray-500 px-2">This project was my final capstone project in college. It's an app that contains quizzes and forums which provides a community based learning environment. My contribution was mostly on the forum aspect in terms of front end and backend, also helped out with the general frontend.</span>
                         <a href="https://quiz-hoot-erzsb9h1b-jrdc12.vercel.app/" target="_blank" rel="noreferrer" class="hover:text-gray-500">Deployment Link</a>
                     </div>
             </div> 
@@ -22,7 +22,7 @@ export const Projects = () => {
                 <img src={projImg2} alt="Project 2" class="w-full h-32 sm:h-48"/>
                 <div>
                     <span class="font-bold">Weather App</span>
-                    <span class="block text-sm text-gray-500">This app shows the usage of a weather api, allowing the user to input a city of their choice and recieving the weather conditions of that city.</span>
+                    <span class="block text-sm text-gray-500 px-2">This app shows the usage of a weather api, allowing the user to input a city of their choice and recieving the weather conditions of that city.</span>
                     <a href="https://github.com/Josephsomethingelse12341/101317769_comp3123_labtest2" target="_blank" rel="noreferrer" class="hover:text-gray-500">Github Link</a>
                 </div>
             </div>
@@ -30,7 +30,7 @@ export const Projects = () => {
                 <img src={projImg3} alt="Project 3" class="w-full h-32 sm:h-48"/>
                 <div>
                     <span class="font-bold">Simple Crud System</span>
-                    <span class="block text-sm text-gray-500">For this assignment, I had to make a crud system, allowing users to login and signup. Letting them create, delete, update employees. Requires a bit of knowledge of apollo to run this project, deployment in the github link.</span>
+                    <span class="block text-sm text-gray-500 px-2">For this assignment, I had to make a crud system, allowing users to login and signup. Letting them create, delete, update employees. Requires a bit of knowledge of apollo to run this project, deployment in the github link.</span>
                     <a href="https://github.com/Josephsomethingelse12341/101317769_COMP3133_Assignment1" target="_blank" rel="noreferrer" class="hover:text-gray-500">Github Link</a>
                 </div>
             </div>
